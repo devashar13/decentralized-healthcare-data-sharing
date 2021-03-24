e0c2ab18214e90c1244cbcc004f0f21d2e226c22
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2021-03-24 18:23:32 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Identicon from 'identicon.js';
+import dvideo from '../dvideo.png'
+
+const Navbar = () => {
+  return (
+    <div>
+            <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow text-monospace">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={dvideo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          &nbsp;DVide0
+        </a>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{/* Account... */}</small>
+            </small>
+              {/* Return Account&Identicon... */}
+              <b className="text-white">0x0</b>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
