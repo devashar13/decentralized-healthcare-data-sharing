a90ae451cb669e5a1f8730a26dc6317a90e0e9ba
Rahul Bhave <rahul@Rahuls-MacBook-Air.local>
2022-06-29 21:59:28 +0530

    working

--- truffle.js
+++ truffle.js
@@ -4,8 +4,8 @@ require('babel-polyfill');
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545,
+      host: "127.0.0.1:7545",
+      port: 7545,
       network_id: "*" 
     },
   },
